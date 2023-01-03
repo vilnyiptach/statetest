ff09ef0930714f8b3d567c99e8a5dcbd4324f0c7
Zoreslav Hrushevskyi <3opik@i.ua>
2023-01-03 17:07:17 +0200

    branch testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppCounter from './AppCounter';
+// import AppCounter from './AppCounter';
 import Form from './FormTest'
 
-ReactDOM.render(<AppCounter darkMode = {true} />, document.getElementById("root"))
-// ReactDOM.render(<Form />, document.getElementById("root"))
+// ReactDOM.render(<AppCounter darkMode = {true} />, document.getElementById("root"))
+ReactDOM.render(<Form />, document.getElementById("root"))
 
